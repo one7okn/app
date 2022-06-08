2ef333031e97b937a97a439c731466e448025d2f
PatRom <bourgade.romain@gmail.com>
2022-06-08 09:28:46 -0400

    Before We3modal

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,40 +3,13 @@ import "./App.scss";
 import { NftOptions, Spinner } from "./components";
 import { INftOptionSummary } from "./models";
 import { getNftOptions } from "./services";
-import logo from "./logo.png";
-import {
-  AppBar,
-  Box,
-  Button,
-  createTheme,
-  IconButton,
-  Menu,
-  MenuItem,
-  ThemeProvider,
-  Toolbar,
-  Typography
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import MenuAppBar from "./components/common/MenuAppBar";
 
 const pages = ["Products", "Pricing", "Blog"];
 
 function App() {
   const [nftOptions, setNftOptions] = useState<INftOptionSummary[]>();
-  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
   useEffect(() => {
     getNftOptions().then((nftOptionSummaries: INftOptionSummary[]) => {
@@ -50,6 +23,16 @@ function App() {
       primary: {
         main: "#0f85cc"
       }
+    },
+    components: {
+      MuiAppBar: {
+        styleOverrides: {
+          colorPrimary: {
+            backgroundColor: "#000000",
+            backgroundImage: "none"
+          }
+        }
+      }
     }
   });
 
@@ -57,47 +40,9 @@ function App() {
 
   return (
     <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <main className="App">
         <MenuAppBar />
-        {/* <AppBar position="sticky" className="App__AppBar">
-          <Toolbar disableGutters={true}>
-            <img alt="One7okn" src={logo} className="App__AppBar__img" />
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit">
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left"
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left"
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" }
-                }}>
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-          </Toolbar>
-        </AppBar> */}
         <NftOptions nftOptions={nftOptions} />
       </main>
     </ThemeProvider>
