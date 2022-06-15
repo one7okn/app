583428e8cc2e2067475e9afa37adb2b486e7ebba
PatRom <43301301+patromain@users.noreply.github.com>
2022-06-15 14:39:17 -0400

    Feature/split (#3)
    * remove html
    
    * move file
    
    * add rinkeby + ropsten

--- src/services/nftOption.ts
+++ src/services/nftOption.ts
@@ -3,7 +3,8 @@ import { ethers } from "ethers";
 import NftOptionContract from "../artifacts/contracts/Option.sol/Option.json";
 
 declare var window: any;
-let NftOptionAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// let NftOptionAddress = "0xe756C0DA1d60acF9CA8d7980D9080cB82283D112"; //ropsten
+let NftOptionAddress = "0x6787E83E5Ad5b6118C21d8882E9BAf427c4a0d55"; //rinkeby
 export const address0 = "0x0000000000000000000000000000000000000000";
 
 export const getNftOptions = async (isMyOption: boolean): Promise<INftOptionSummary[]> => {
@@ -94,6 +95,7 @@ export const executeNftOption = async (tokenId: string): Promise<void> => {
 export const getMyAddress = async (): Promise<string> => {
   if (typeof window.ethereum !== "undefined") {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const t = await provider.getSigner().getAddress();
     return await provider.getSigner().getAddress();
   }
 
